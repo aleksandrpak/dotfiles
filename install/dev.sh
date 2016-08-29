0b3d611a84b5f83d8690e1a193982eecaa2c92b5
Aleksandr Pak <alekspak@icloud.com>
2016-08-29 21:36:05 +0200

    removed unused langs

--- install/dev.sh
+++ install/dev.sh
@@ -12,17 +12,6 @@ get go
 mkdir -p ~/Projects/gocode
 go get -u github.com/nsf/gocode
 
-# Installing Haskell
-get ghc
-get haskell-stack
-get cabal-install
-cabal update
-cabal install happy
-cabal install ghc-mod
-
-# Installing mono
-get mono
-
 # Installing python
 get python
 get python3
@@ -36,16 +25,6 @@ pip install grip
 # Installing nvim bundles
 pip install neovim --upgrade
 echo "iExecute :PlugInstall and close vim" | nvim
-
-# Installing Java & Scala
-get_desktop install java
-get scala
-get sbt
-curl https://raw.githubusercontent.com/n8han/conscript/master/setup.sh | sh
-
-# Building auto-complete
-cd ~/.config/nvim/plugged/YouCompleteMe
-./install.py --clang-completer --gocode-completer --racer-completer
 
 # iTerm2 shell integration
 curl -L https://iterm2.com/misc/install_shell_integration.sh | bash
