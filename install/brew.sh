5eedc17fc31e12cee90e9bb17a8a00db6d4140d2
Aleksandr Pak <aleksandrpak@users.noreply.github.com>
2018-02-27 23:30:33 +0100

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -19,10 +19,4 @@ get_desktop mattr-slate
 
 # Font setup
 get_desktop caskroom/fonts/font-hack
-
-cd ~/Library/Fonts
-if [ ! -e "Droid Sans Mono for Powerline Nerd Font Complete.otf" ]
-then
-    curl -fLo "Droid Sans Mono for Powerline Nerd Font Complete.otf" https://raw.githubusercontent.com/ryanoasis/nerd-fonts/master/patched-fonts/DroidSansMono/complete/Droid%20Sans%20Mono%20for%20Powerline%20Nerd%20Font%20Complete.otf
-fi
-
+get_desktop font-droid-sans-mono-for-powerline
