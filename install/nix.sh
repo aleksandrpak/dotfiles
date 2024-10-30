5653af8f76086bf13d961f15d590b4fcbf08ab63
Aleksandr Pak <alekspak@icloud.com>
2024-10-30 18:25:20 +0100

    Add activation command

--- install/nix.sh
+++ install/nix.sh
@@ -2,5 +2,6 @@
 
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 sudo chown -R $(whoami):$(groups | awk '{print $1}') ~/.cache
+source '/nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh'
 cd ~/.config/nix
 nix run nix-darwin -- switch --flake .#personal
