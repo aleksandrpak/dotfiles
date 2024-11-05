427ce80c61dc0c3aef6666391288ad6f38984374
Aleksandr Pak <alekspak@icloud.com>
2024-11-05 22:19:31 +0000

    add nix update

--- update.sh
+++ update.sh
@@ -14,6 +14,7 @@ echo "Updating git submodules"
 git submodule update --remote --merge
 
 echo "Updating flake"
+nix-channel --update
 
 if [ "$(uname)" == "Darwin" ]; then
     darwin-rebuild switch --flake ~/.dotfiles/config/home-manager
